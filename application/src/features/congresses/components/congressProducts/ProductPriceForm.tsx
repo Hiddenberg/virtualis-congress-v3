09c5595a8524cf8413ceea73da28c66c3c49c3c2
hiddenberg <victor_hals@hotmail.com>
2026-01-21 11:52:31 -0600

    adding necessary parts of the product price form

--- application/src/features/congresses/components/congressProducts/ProductPriceForm.tsx
+++ application/src/features/congresses/components/congressProducts/ProductPriceForm.tsx
@@ -1,10 +1,14 @@
 "use client";
 
-import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useState, useTransition } from "react";
+import toast from "react-hot-toast";
+import { createCongressProductPriceAction } from "../../serverActions/congressProductPricesActions";
 import { NewProductPriceData } from "../../types/congressProductPricesTypes";
 import { CongressProductRecord, NewCongressProductData } from "../../types/congressProductsTypes";
 
 export default function ProductPriceForm({ productId }: { productId: CongressProductRecord["id"] }) {
+   const [isPending, startTransition] = useTransition();
    const [priceFormData, setPriceFormData] = useState<NewProductPriceData>({
       name: "",
       currency: "mxn",
@@ -12,6 +16,7 @@ export default function ProductPriceForm({ productId }: { productId: CongressPro
       requiresCredentialValidation: false,
       product: productId,
    });
+   const router = useRouter();
 
    const [errors, setErrors] = useState<Partial<Record<keyof NewProductPriceData, string>>>({});
 
@@ -20,6 +25,19 @@ export default function ProductPriceForm({ productId }: { productId: CongressPro
    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => {
       const { name, value } = e.target;
       setPriceFormData({ ...priceFormData, [name]: value });
+   };
+
+   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      startTransition(async () => {
+         const response = await createCongressProductPriceAction(priceFormData);
+         if (!response.success) {
+            toast.error(response.errorMessage);
+            return;
+         }
+         toast.success("Precio del producto creado correctamente");
+         router.push(`/congress-admin/products/${productId}/prices`);
+      });
    };
 
    return <form action=""></form>;
