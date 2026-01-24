1750ccb3b82e446fd765b9e649ae69509d7dd935
hiddenberg <victor_hals@hotmail.com>
2026-01-24 12:19:39 -0600

    adding the initial structure of how the payment form should work

--- application/src/components/payment-page/prices/PricePaymentForm.tsx
+++ application/src/components/payment-page/prices/PricePaymentForm.tsx
@@ -1,7 +1,36 @@
 "use client";
 
+import { useState, useTransition } from "react";
+import toast from "react-hot-toast";
+import { obtainPriceStripeCheckoutUrlAction } from "@/features/congresses/serverActions/congressProductsActions";
 import type { ProductPriceRecord } from "@/features/congresses/types/congressProductPricesTypes";
 
 export default function PricePaymentForm({ price }: { price: ProductPriceRecord }) {
+   const [isLoading, startTransition] = useTransition();
+   const [paymentFormData, setPaymentFormData] = useState<{
+      credentialFile?: File;
+      includeRecordings: boolean;
+   }>({
+      includeRecordings: false,
+   });
+
+   const handlePay = () => {
+      startTransition(async () => {
+         const response = await obtainPriceStripeCheckoutUrlAction({
+            priceId: price.id,
+            includeRecordings: true,
+         });
+
+         if (!response.success) {
+            toast.error(response.errorMessage);
+            return;
+         }
+
+         const checkoutLink = response.data;
+         // Redirect to the checkout link
+         window.location.href = checkoutLink;
+      });
+   };
+
    return <div>PricePaymentForm</div>;
 }
