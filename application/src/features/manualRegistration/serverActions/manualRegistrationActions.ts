e26951f2a1eb96e979f54b3d1cedf2ebb3860f62
hiddenberg <victor_hals@hotmail.com>
2026-01-28 11:59:42 -0600

    refactoring staff user creation action to use a batch and correctly register the user to the congress

--- application/src/features/manualRegistration/serverActions/manualRegistrationActions.ts
+++ application/src/features/manualRegistration/serverActions/manualRegistrationActions.ts
@@ -1,9 +1,15 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
-import { registerUserToLatestCongress } from "@/features/congresses/services/congressRegistrationServices";
-import { checkIfUserExists, createUser, type NewUserData } from "@/features/users/services/userServices";
-import type { UserRecord } from "@/features/users/types/userTypes";
+import { getLatestCongress } from "@/features/congresses/services/congressServices";
+import type { CongressRegistration } from "@/features/congresses/types/congressRegistrationTypes";
+import { sendPlatformRegistrationConfirmationEmail } from "@/features/emails/services/emailSendingServices";
+import { getOrganizationFromSubdomain } from "@/features/organizations/services/organizationServices";
+import { generateRandomId } from "@/features/staggeredAuth/utils/passwordsGenerator";
+import { checkIfUserExists } from "@/features/users/services/userServices";
+import type { User, UserRecord } from "@/features/users/types/userTypes";
+import { dbBatch } from "@/libs/pbServerClientNew";
+import PB_COLLECTIONS from "@/types/constants/pocketbaseCollections";
 import {
    // fulfillManualCongressRegistration,
    searchUsersRegisteredToCurrentCongress,
@@ -94,17 +100,44 @@ export interface StaffNewUserFormData {
    additionalEmail2?: string;
 }
 
-export async function staffCreateAttendantUserAction(form: StaffNewUserFormData): Promise<BackendResponse<{ user: UserRecord }>> {
+export async function staffCreateAttendantUserAction(form: StaffNewUserFormData): Promise<BackendResponse<null>> {
    try {
       const exists = await checkIfUserExists(form.email);
       if (exists) {
          return {
             success: false,
-            errorMessage: "Este correo ya está registrado, por favor usa otro",
+            errorMessage: "Este correo ya está registrado en la plataforma",
          };
       }
 
-      const newUser = await createUser({
+      if (form.additionalEmail1) {
+         const additionalEmail1Exists = await checkIfUserExists(form.additionalEmail1);
+         if (additionalEmail1Exists) {
+            return {
+               success: false,
+               errorMessage: `Este correo adicional ${form.additionalEmail1} ya está registrado en la plataforma`,
+            };
+         }
+      }
+
+      if (form.additionalEmail2) {
+         const additionalEmail2Exists = await checkIfUserExists(form.additionalEmail2);
+         if (additionalEmail2Exists) {
+            return {
+               success: false,
+               errorMessage: `Este correo adicional ${form.additionalEmail2} ya está registrado en la plataforma`,
+            };
+         }
+      }
+
+      const organization = await getOrganizationFromSubdomain();
+      const congress = await getLatestCongress();
+      const batch = dbBatch();
+
+      const newUserId = generateRandomId();
+      batch.collection(PB_COLLECTIONS.USERS).create({
+         id: newUserId,
+         organization: organization.id,
          name: form.name,
          email: form.email,
          phoneNumber: form.phoneNumber,
@@ -112,17 +145,29 @@ export async function staffCreateAttendantUserAction(form: StaffNewUserFormData)
          additionalEmail1: form.additionalEmail1,
          additionalEmail2: form.additionalEmail2,
          role: "attendant",
-      } as NewUserData);
+      } satisfies User & { id: string });
 
-      await registerUserToLatestCongress(newUser.id);
+      // Register the user to the congress
+      const userRegistrationId = generateRandomId();
+      batch.collection(PB_COLLECTIONS.CONGRESS_REGISTRATIONS).create({
+         id: userRegistrationId,
+         organization: organization.id,
+         user: newUserId,
+         congress: congress.id,
+         hasAccessToRecordings: false,
+         paymentConfirmed: false,
+         registrationType: "regular",
+      } satisfies CongressRegistration & { id: string });
+
+      await batch.send();
+
+      await sendPlatformRegistrationConfirmationEmail(newUserId);
 
       revalidatePath("/manual-registration");
 
       return {
          success: true,
-         data: {
-            user: newUser,
-         },
+         data: null,
       };
    } catch (error) {
       if (error instanceof Error) {
